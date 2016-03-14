34967c1a95d920c914ffa785d8bfa2693a8def41
Mei Huang <mhuang5@stuy.edu>
2016-03-14 15:35:11 -0400

    changes

--- List.java
+++ List.java
@@ -1,6 +1,7 @@
 public interface List { 
-    public boolean add (String x); 
+    public boolean add(String x); 
     public String get(int i); 
-    public String set(int i, String x); 
-    public int size(); 
-} 
+    public String set(int i, String x);
+    public int size();
+}
+
