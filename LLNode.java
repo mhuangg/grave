99178c06fb34e35bd55b7eb04684361310a1d6ba
Mei Huang <mhuang5@stuy.edu>
2016-03-15 15:34:31 -0400

    Changes

--- LLNode.java
+++ LLNode.java
@@ -1,8 +1,4 @@
 public class LLNode { 
-    String a = "Hunter"; 
-
-    a node has a value and linkage to next node << alias to another node. 
-
-a node can get built -- return its cargo etc etc etc in NB << accessors to the mutators. 
-
+    private String _node; 
+    private LLNode _nextNode; 
 
